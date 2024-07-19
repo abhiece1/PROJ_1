4adc9b096b116cd6ec51845dc92c4f4261f0dc84
abhiece1 <112398043+abhiece1@users.noreply.github.com>
2024-07-19 11:33:35 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,7 +4,6 @@ export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   params: {
-    relatedToVideoId: '7ghhRHRP6t4',
     part: 'id,snippet',
     type: 'video',
     maxResults: '50'
